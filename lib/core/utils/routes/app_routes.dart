be406ff23ad29cadac2002d64a1ad2d5960d0f8a
Ziad <zslah2800@gmail.com>
2025-02-28 17:24:02 +0200

    adding go router to the project

---
+++ lib/core/utils/routes/app_routes.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+import '../../../features/splash/presentation/views/splash_view.dart';
+
+abstract class AppRoutes {
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class TalabatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Padding(
