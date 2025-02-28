76d19f68b0830abde69b1298202c31956fd4391a
Ziad <zslah2800@gmail.com>
2025-02-28 14:32:29 +0200

    app_images and app_colors have been finished

---
+++ lib/core/utils/styles/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color mainColor = Color(0xffFF6100);
+  static const Color greyColor = Color(0xffD9D9D9);
+  static const Color inactiveButton = Color(0xffffcdb2);
+}
---
+++ lib/core/utils/styles/app_images.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAppIcon
+  /// assets/images/appIcon.png
+  static const String imagesAppIcon = "assets/images/appIcon.png";
+
+  /// Assets for imagesDelivery
+  /// assets/images/delivery.png
+  static const String imagesDelivery = "assets/images/delivery.png";
+
+  /// Assets for imagesExitIcon
+  /// assets/images/exitIcon.png
+  static const String imagesExitIcon = "assets/images/exitIcon.png";
+
+  /// Assets for imagesFlowers
+  /// assets/images/flowers.png
+  static const String imagesFlowers = "assets/images/flowers.png";
+
+  /// Assets for imagesGroceries
+  /// assets/images/groceries.png
+  static const String imagesGroceries = "assets/images/groceries.png";
+
+  /// Assets for imagesHealth
+  /// assets/images/health.png
+  static const String imagesHealth = "assets/images/health.png";
+
+  /// Assets for imagesHomeImage
+  /// assets/images/homeImage.png
+  static const String imagesHomeImage = "assets/images/homeImage.png";
+
+  /// Assets for imagesMoreShops
+  /// assets/images/moreShops.png
+  static const String imagesMoreShops = "assets/images/moreShops.png";
+
+  /// Assets for imagesOrders
+  /// assets/images/orders.png
+  static const String imagesOrders = "assets/images/orders.png";
+
+  /// Assets for imagesPayCard
+  /// assets/images/payCard.png
+  static const String imagesPayCard = "assets/images/payCard.png";
+
+  /// Assets for imagesTalabat
+  /// assets/images/talabat.png
+  static const String imagesTalabat = "assets/images/talabat.png";
+
+  /// Assets for imagesTalabatMart
+  /// assets/images/talabatMart.png
+  static const String imagesTalabatMart = "assets/images/talabatMart.png";
+}
