f25c470880618898f49ba62bc1695b0859b2beee
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-01 13:19:32 -0800

    create login view and edit round two

--- lib/core/utils/routes/app_routes.dart
+++ lib/core/utils/routes/app_routes.dart
@@ -1,9 +1,12 @@
 import 'package:go_router/go_router.dart';
+
 import '../../../features/auth/presentation/views/auth_option_view.dart';
+import '../../../features/auth/presentation/views/login_view.dart';
 import '../../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRoutes {
   static const String authOptionView = '/authOptionView';
+  static const String loginView = '/loginView';
 
   static final router = GoRouter(
     routes: [
@@ -14,6 +17,10 @@ abstract class AppRoutes {
       GoRoute(
         path: authOptionView,
         builder: (context, state) => const AuthOptionView(),
+      ),
+      GoRoute(
+        path: loginView,
+        builder: (context, state) => const LoginView(),
       ),
     ],
   );
