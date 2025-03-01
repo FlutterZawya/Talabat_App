f7fc9ef3fdb027ff219457b58f5a8901c8a5cf66
Ziad <zslah2800@gmail.com>
2025-03-01 23:15:44 +0200

    round two

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:talabat_app/main.dart';
+import 'package:talabat_app/talabat_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
