414a18c219c156719eeaa0a380ab68085a852ea5
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-02 21:52:30 +0200

    create custom text form field

---
+++ lib/core/utils/widgets/custom_text_form_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String hintText;
+  final String? Function(String?)? validator;
+  final bool? obscureText;
+  final TextEditingController? controller;
+  final Widget? suffix;
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    this.validator,
+    this.obscureText = false,
+    this.controller, this.suffix,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller, 
+      obscureText: obscureText ?? false,
+      validator: validator,
+
+      decoration: InputDecoration(
+        hintText: hintText,
+        suffix: suffix,
+      ),
+      
+    );
+  }
+}
