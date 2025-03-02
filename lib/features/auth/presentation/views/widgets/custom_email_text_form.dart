6901e3371b119972a31dc630c0aa995ca3b40178
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-02 21:53:49 +0200

    create custom email text form field

---
+++ lib/features/auth/presentation/views/widgets/custom_email_text_form.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:talabat_app/core/utils/functions/text_form_validation.dart';
+import 'package:talabat_app/core/utils/widgets/custom_text_form_field.dart';
+
+class CustomEmailTextFormField extends StatelessWidget {
+  const CustomEmailTextFormField({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      hintText: 'Email Address',
+      controller: emailController,
+      validator: (email) {
+        return validateEmail(email);
+      },
+    );
+  }
+}
