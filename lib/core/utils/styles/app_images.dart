154721a675889f00bb574562f6cd81d98607fe8c
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-03 11:14:26 -0800

    add complete lottie json and refactor flutter_assets gen

--- lib/core/utils/styles/app_images.dart
+++ lib/core/utils/styles/app_images.dart
@@ -1,77 +1,43 @@
 // ignore_for_file: prefer_single_quotes
 class AppImages {
   AppImages._();
-  
-  /// Assets for imagesAppIcon
-  /// assets/images/appIcon.png
+
+  static const String animationsCompleteLottie =
+      "assets/animations/complete_lottie.json";
+
   static const String imagesAppIcon = "assets/images/appIcon.png";
 
-  /// Assets for imagesApple
-  /// assets/images/apple.svg
   static const String imagesApple = "assets/images/apple.svg";
 
-  /// Assets for imagesAuthOption
-  /// assets/images/authOption.png
   static const String imagesAuthOption = "assets/images/authOption.png";
 
-  /// Assets for imagesAuthView
-  /// assets/images/authView.png
   static const String imagesAuthView = "assets/images/authView.png";
 
-  /// Assets for imagesDelivery
-  /// assets/images/delivery.png
   static const String imagesDelivery = "assets/images/delivery.png";
 
-  /// Assets for imagesEmail
-  /// assets/images/email.svg
   static const String imagesEmail = "assets/images/email.svg";
 
-  /// Assets for imagesExitIcon
-  /// assets/images/exitIcon.png
   static const String imagesExitIcon = "assets/images/exitIcon.png";
 
-  /// Assets for imagesFacebook
-  /// assets/images/facebook.svg
   static const String imagesFacebook = "assets/images/facebook.svg";
 
-  /// Assets for imagesFlowers
-  /// assets/images/flowers.png
   static const String imagesFlowers = "assets/images/flowers.png";
 
-  /// Assets for imagesGoogle
-  /// assets/images/google.svg
   static const String imagesGoogle = "assets/images/google.svg";
 
-  /// Assets for imagesGroceries
-  /// assets/images/groceries.png
   static const String imagesGroceries = "assets/images/groceries.png";
 
-  /// Assets for imagesHealth
-  /// assets/images/health.png
   static const String imagesHealth = "assets/images/health.png";
 
-  /// Assets for imagesHomeImage
-  /// assets/images/homeImage.png
   static const String imagesHomeImage = "assets/images/homeImage.png";
 
-  /// Assets for imagesMoreShops
-  /// assets/images/moreShops.png
   static const String imagesMoreShops = "assets/images/moreShops.png";
 
-  /// Assets for imagesOrders
-  /// assets/images/orders.png
   static const String imagesOrders = "assets/images/orders.png";
 
-  /// Assets for imagesPayCard
-  /// assets/images/payCard.png
   static const String imagesPayCard = "assets/images/payCard.png";
 
-  /// Assets for imagesTalabat
-  /// assets/images/talabat.png
   static const String imagesTalabat = "assets/images/talabat.png";
 
-  /// Assets for imagesTalabatMart
-  /// assets/images/talabatMart.png
   static const String imagesTalabatMart = "assets/images/talabatMart.png";
 }
-
