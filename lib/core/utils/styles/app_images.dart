81c42f6cc967afda93d4cc8d2a9093db14c0ecb9
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-04 11:57:57 -0800

    add the icons of bottom navigation bar

--- lib/core/utils/styles/app_images.dart
+++ lib/core/utils/styles/app_images.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: prefer_single_quotes
 class AppImages {
   AppImages._();
+  
+  static const String animationsCompleteLottie = "assets/animations/complete_lottie.json";
 
-  static const String animationsCompleteLottie =
-      "assets/animations/complete_lottie.json";
+  static const String imagesActiveAccountIcon = "assets/images/active_account_icon.svg";
+
+  static const String imagesActiveSearchIcon = "assets/images/active_search_icon.svg";
+
+  static const String imagesActiveTalabatIcon = "assets/images/active_talabat_icon.svg";
 
   static const String imagesAppIcon = "assets/images/appIcon.png";
 
@@ -30,6 +36,12 @@ class AppImages {
 
   static const String imagesHomeImage = "assets/images/homeImage.png";
 
+  static const String imagesInactiveAccountIcon = "assets/images/inactive_account_icon.svg";
+
+  static const String imagesInactiveSearchIcon = "assets/images/inactive_search_icon.svg";
+
+  static const String imagesInactiveTalabatIcon = "assets/images/inactive_talabat_icon.svg";
+
   static const String imagesMoreShops = "assets/images/moreShops.png";
 
   static const String imagesOrders = "assets/images/orders.png";
@@ -40,3 +52,4 @@ class AppImages {
 
   static const String imagesTalabatMart = "assets/images/talabatMart.png";
 }
+
