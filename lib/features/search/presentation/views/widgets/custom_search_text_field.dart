a9f12754bf20ef4379699f888c0f1fbad749aaf9
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-05 11:00:02 -0800

    Custom Search Text Field

---
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles/app_colors.dart';
+import '../../../../../core/utils/styles/app_styles.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField(
+      {super.key, this.onChanged, required this.hintText});
+  final Function(String)? onChanged;
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextField(
+        style: AppStyles.medium18,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.main),
+          ),
+          hintText: hintText,
+          hintStyle: AppStyles.regular14,
+          prefixIcon: Icon(Icons.search),
+        ),
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
