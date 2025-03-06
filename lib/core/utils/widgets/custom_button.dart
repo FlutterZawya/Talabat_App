afa5005e38834a6655bdc36e0891452550396ab8
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-06 04:28:51 -0800

    refactor custom button

--- lib/core/utils/widgets/custom_button.dart
+++ lib/core/utils/widgets/custom_button.dart
@@ -2,27 +2,37 @@ import 'package:flutter/material.dart';
 
 import '../styles/app_colors.dart';
 import '../styles/app_styles.dart';
+
 class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
     required this.text,
     this.onPressed,
+    this.backgroundColor = AppColors.main,
+    this.textColor = AppColors.white,
+    this.borderRadius = 10,
+    this.height = 55,
   });
   final String text;
   final void Function()? onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final double borderRadius;
+  final double height;
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       onPressed: onPressed,
       minWidth: double.infinity,
-      height: 55,
+      height: height,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(borderRadius),
+        side: BorderSide(color: textColor, width: 1.5),
       ),
-      color: AppColors.main,
+      color: backgroundColor,
       child: Text(
         text,
-        style: AppStyles.medium14.copyWith(color: AppColors.white),
+        style: AppStyles.medium14.copyWith(color: textColor),
       ),
     );
   }
