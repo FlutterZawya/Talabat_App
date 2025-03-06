a2a684707ca0401a2904c5f8a6420cae0920752f
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-06 21:35:45 +0200

    create account setting item

--- lib/features/account/presentation/views/account_settings_view.dart
+++ lib/features/account/presentation/views/account_settings_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:talabat_app/features/auth/presentation/views/widgets/custom_auth_app_bar.dart';
 
+import 'widgets/account_setting_item.dart';
+
 class AccountSettingsView extends StatelessWidget {
   const AccountSettingsView({super.key});
 
@@ -8,6 +10,15 @@ class AccountSettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAuthAppBar(context, title: 'Settings'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          AccountSettingItem()
+        ],
+      ),
     );
   }
 }
+
