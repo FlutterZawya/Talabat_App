cec8cb220a00458ad0831f3a47a0480aa3c78673
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-06 03:34:06 -0800

    Search View (AppBar+TabBar) [Done]

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,10 +1,35 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../../../../core/utils/styles/app_colors.dart';
+import '../../../../core/utils/styles/app_styles.dart';
 import 'widgets/custom_search_text_field.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   const SearchView({super.key});
+
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the TabController with the number of tabs
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // Dispose the TabController when the widget is removed
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +39,44 @@ class SearchView extends StatelessWidget {
           hintText: 'Search food, groceries and more',
           onChanged: (value) {},
         ),
-        toolbarHeight: 60,
+        // toolbarHeight: 60,
         backgroundColor: AppColors.white,
         flexibleSpace: Container(color: AppColors.white),
+
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: AppColors.main,
+          labelStyle: AppStyles.bold14.copyWith(color: AppColors.main),
+          isScrollable: true,
+          indicatorPadding: EdgeInsets.zero,
+          onTap: (value) {
+            log(value.toString()); // value is the index of the tab
+          },
+          labelPadding: EdgeInsets.only(right: 20, left: 0),
+          padding: EdgeInsets.zero,
+          labelColor: AppColors.main,
+          dividerColor: AppColors.grey400,
+          enableFeedback: false, //
+          tabAlignment: TabAlignment.startOffset,
+
+          tabs: const [
+            Tab(text: 'Food'),
+            Tab(text: 'Groceries'),
+            Tab(text: 'Health & wellness'),
+            Tab(text: 'Flowers'),
+            Tab(text: 'More shops'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          SizedBox(), //FoodView(),
+          SizedBox(), //GroceriesView(),
+          SizedBox(), //HealthView(),
+          SizedBox(), //FlowersView(),
+          SizedBox(), //MoreShopsView(),
+        ],
       ),
     );
   }
