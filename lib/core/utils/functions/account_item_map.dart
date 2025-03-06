9468e61f46dc74534a03ab50488c270fd2301dd6
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-03-06 17:28:39 +0200

    create account item map

--- lib/core/utils/functions/account_item_map.dart
+++ lib/core/utils/functions/account_item_map.dart
@@ -1,4 +1,13 @@
+import '../styles/app_icons.dart';
+
 Map<String, String> selectAccountItem() {
-  Map<String, String> items = {"": ""};
+  Map<String, String> items = {
+    AppIcons.notepad: "Your Orders",
+    AppIcons.arcticonsOfferup: "Offers",
+    AppIcons.notifications: "Notifications",
+    AppIcons.uiwPay: "talabat pay",
+    AppIcons.mdiWhistleOutline: "Get help",
+    AppIcons.mdiAboutCircleOutline: "About app"
+  };
   return items;
 }
