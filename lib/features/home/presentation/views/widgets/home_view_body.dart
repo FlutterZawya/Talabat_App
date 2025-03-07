293a7c1133381dec5d5d8873ee2b442fa6b0f16b
Ziad <zslah2800@gmail.com>
2025-03-07 15:14:43 +0200

    round four and the last round

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,41 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return  SingleChildScrollView(
+      child: Column(
+        children: [
+          // image.assets('Welcome to the Home Page'),
+          SizedBox(height: 16),
+          Text('This is the HomeViewBody'),
+          SizedBox(height: 16),
+          Text('Here are some quick links for you'),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: Text('Link 1'),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Text('Link 2'),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: Text('Link 3'),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Text('Link 4'),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
+    );
   }
 }
