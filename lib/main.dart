8ac86f45385b13dfcca1fd625457e7bf50c9fc49
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-03-13 04:34:57 -0700

    set the screens of device not rotated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'talabat_app.dart';
 
-void main() {
-  runApp(
-    DevicePreview(
-      // enabled: false,
-      builder: (context) => const TalabatApp(),
-    ),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // this step used to set device not rotated
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (_) {
+      runApp(
+        DevicePreview(
+          // enabled: false,
+          builder: (context) => const TalabatApp(),
+        ),
+      );
+    },
   );
 }
